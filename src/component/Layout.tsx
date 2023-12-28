e18f86d5287862ffbdaa2d87b9066a7fa33c0999
landot <timowland@gmail.com>
2023-12-28 14:28:53 -0800

    chore: update icon

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -3,6 +3,7 @@ import Footer from './Footer';
 import Header from './Header';
 import styled from 'styled-components';
 import { GlobalStyles } from '@/style/GlobalStyles.styles';
+import Head from 'next/head';
 
 export const LayoutStyles = styled.div`
     display: flex;
@@ -23,6 +24,9 @@ function Layout(props: LayoutProps) {
     return (
         <>
             <GlobalStyles />
+            <Head>
+                <link rel="shortcut icon" href="/icon.png" />
+            </Head>
             <LayoutStyles>
                 <Header handleCartClick={props.handleCartClick} />
                 <main role='main'>
