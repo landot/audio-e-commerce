10abdcacdebc6100f334e18788157125c9749f10
landot <timowland@gmail.com>
2023-12-29 10:56:59 -0800

    feat: add playwright test and add to precommit hook

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,36 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
 })
 
-// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ["./src/**"],
-  "coverageThreshold": {
-    "global": {
-      "lines": 70
-    }
-  },
-  coveragePathIgnorePatterns: [
-    "<rootDir>/src/style/", 
-    "<rootDir>/src/app/",
-    "<rootDir>/src/pages/", // remove this later
-    "\\.stories\\.",
-  ],
+    // Add more setup options before each test is run
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+    moduleDirectories: ['node_modules', '<rootDir>/'],
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testPathIgnorePatterns: [
+        "<rootDir>/e2e",
+    ],
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/**"],
+    "coverageThreshold": {
+        "global": {
+            "lines": 70
+        }
+    },
+    coveragePathIgnorePatterns: [
+        "<rootDir>/src/style/", 
+        "<rootDir>/src/app/",
+        "<rootDir>/src/pages/", // remove this later
+        "<rootDir>/e2e",
+        "\\.stories\\.",
+    ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
